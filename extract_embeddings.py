5ba185c152432e5bbf9d785abc546d476b41e95f
jaclin <jaclin@umich.edu>
2026-02-07 20:25:46 -0500

    fix

--- extract_embeddings.py
+++ extract_embeddings.py
@@ -3,7 +3,6 @@ import torchxrayvision as xrv
 import skimage.io
 import torchvision.transforms as transforms
 import torch.nn.functional as F
-import numpy as np
 
 class extractor():
     def __init__(self, healthy_embedding_path: str="healthy_mean_weight/healthy_mean_embedding.pt", device: str="cuda"):
@@ -11,10 +10,10 @@ class extractor():
         self.model = xrv.models.DenseNet(weights="densenet121-res224-all").to(self.device)
         self.model.eval()
 
-        # this is the healthy embedding from the NIH dataset
         self.healthy_embedding = torch.load(healthy_embedding_path).to(self.device)
+        self.healthy_emb = self.healthy_embedding.unsqueeze(0)  # shape [1, 1024]
 
-        # the transformation for dense net
+        # transformation for the nn
         self.transform = transforms.Compose([
             xrv.datasets.XRayCenterCrop(),
             xrv.datasets.XRayResizer(224)
@@ -22,25 +21,25 @@ class extractor():
 
     def process(self, img_path: str):
         img = skimage.imread(img_path)
-        # this is the normalization process + converting for pytorch
         img = img.mean(2)[None, ...]
         img = self.transform(img)
         img = torch.from_numpy(img).float()
         return img.to(self.device)
 
     def compute_embedding(self, img: torch.Tensor) -> torch.Tensor:
-        # compute the embedding
         with torch.no_grad():
-            emb = self.model.features(img[None, ...]) # note: we need to add a batch dimension
+            emb = self.model.features(img[None, ...])
         return emb
 
-    def compute_unhealthy_score(self, img_path: str, metric: str = "cosine"):
+    def compute_unhealthy_score(self, img_path: str, metric: str = "cosine") -> float:
         img = self.process(img_path)
         embedding = self.compute_embedding(img)
 
         if metric == "cosine":
-            score = 1 - F.cosine_similarity(emb, self.healthy_embedding)
+            score = 1 - F.cosine_similarity(embedding, self.healthy_emb)
         elif metric == "euclidean":
-            score = F.pairwise_distance(emb, self.healthy_embedding)
-        
-        return score
+            score = F.pairwise_distance(embedding, self.healthy_emb)
+        else:
+            raise ValueError(f"Unsupported metric: {metric}")
+
+        return score.item()
